4dde334ed43d95bc9ba8c70b6f8553d2cf8fbb80
Hiyo2504 <90575752+Hiyo2504@users.noreply.github.com>
2021-09-13 15:14:06 -0700

    Add files via upload

---
+++ FormularioRegistro/form.php
@@ -0,0 +1,28 @@
+<form name="formulario" method="post" action="formulario.php">
+
+Nombre: <input type="text" name="nombre" value="">
+Apellidos: <input type="text" name="apellido" value="">
+     <select name="lista">
+     <form action ="formulario.php" method="post" name="operacion">
+     <label>Selecione una ciudad:<br/></label>
+     <option value="Guaymas">Guaymas</option>
+            <option value="Hermosillo">Hermosillo</option>
+            <option value="Nogales">Nogales</option>
+            <option value="Navojoa">Navojoa</option>
+            <option value="Hutabampo">Hutabampo</option>
+            <option value="Obregon">Obregon</option>
+            <option value="Empalme">Empalme</option>
+            <option value="Cananea">Cananea</option>
+        </select>
+Edad: <input type="text" name="edad" value="">
+Telefono: <input type="text" name="telefono" value="">
+correo: <input type="text" name="correo" value="">
+     <select name="list">
+     <label>Selecione estado civil:<br/></label>
+           <form action ="formulario.php" method="post" name="estado">
+           <option value="Soltero">Soltero</option>
+           <option value="Casado">Casado</option>
+           </select>
+<input type="submit" />
+
+</form>
---
+++ FormularioRegistro/formulario.php
@@ -0,0 +1,38 @@
+
+
+<?php
+
+$nombre = $_POST['nombre'];
+echo "Nombre: ";
+echo   $nombre;
+
+$apellido = $_POST['apellido'];
+echo "<br> Apellido: ";
+echo $apellido;
+
+
+$edad = $_POST['edad'];
+echo "<br>  Edad: ";
+echo $edad;
+
+$telefono = $_POST['telefono'];
+echo " <br> Telefono: ";
+echo $telefono;
+
+$correo = $_POST['correo'];
+echo " <br> Correo: ";
+echo $correo;
+
+$operacion = $_POST ['lista'];
+echo " <br> Ciudad: ";
+echo $operacion;
+
+$estado = $_POST ['list'];
+echo " <br> Estado Civil: ";
+echo $estado;
+
+
+
+
+
+?>
